be442513e6899e5c98e333b62db73989cc8f102c
devhasibulislam <devhasibulislam@gmail.com>
2023-12-18 17:08:28 +0600

    initialize redux store

---
+++ client/app/store.js
@@ -0,0 +1,29 @@
+/**
+ * Title: Write a program using JavaScript on Store
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 18, December 2023
+ */
+
+import { laparisApi } from "@/services/laparis";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+export const store = configureStore({
+  reducer: {
+    [laparisApi.reducerPath]: laparisApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(laparisApi.middleware),
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+setupListeners(store.dispatch);
