74e26dcae04eb9bfb7ac28dad0f94931ddd72b41
devhasibulislam <devhasibulislam@gmail.com>
2023-12-18 17:08:18 +0600

    integrate redux toolkit and initialize next ui

---
+++ client/app/providers.jsx
@@ -0,0 +1,28 @@
+/**
+ * Title: Write a program using JavaScript on Providers
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 18, December 2023
+ */
+
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { Provider } from "react-redux";
+import { store } from "./store";
+
+export function Providers({ children }) {
+  return (
+    <NextUIProvider>
+      <Provider store={store}>{children}</Provider>
+    </NextUIProvider>
+  );
+}
