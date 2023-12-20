7bb6eb51e2e5a432bf1f12e4c28ec817240cfb05
devhasibulislam <devhasibulislam@gmail.com>
2023-12-20 17:19:36 +0600

    dummy products data initialized

---
+++ client/data/products.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    _id: 1,
+    name: "Canvas Laundry Cart",
+    price: 200,
+    thumbnail: "/products/product1.webp",
+  },
+  {
+    _id: 2,
+    name: "Tiered Wire Basket",
+    price: 300,
+    thumbnail: "/products/product2.webp",
+  },
+  {
+    _id: 3,
+    name: "Smith Journal 13",
+    price: 100,
+    thumbnail: "/products/product3.webp",
+  },
+  {
+    _id: 4,
+    name: "Orbit Terrarium - Small",
+    price: 400,
+    thumbnail: "/products/product4.webp",
+  },
+  {
+    _id: 5,
+    name: "Able Brewing System",
+    price: 200,
+    thumbnail: "/products/product5.webp",
+  },
+  {
+    _id: 6,
+    name: "Orbit Terrarium - Large",
+    price: 150,
+    thumbnail: "/products/product6.webp",
+  },
+  {
+    _id: 7,
+    name: "Orbit Terrarium - Medium",
+    price: 250,
+    thumbnail: "/products/product7.webp",
+  },
+  {
+    _id: 8,
+    name: "Utility Caddy",
+    price: 300,
+    thumbnail: "/products/product8.webp",
+  },
+  {
+    _id: 9,
+    name: "Oak Cheese Grater",
+    price: 200,
+    thumbnail: "/products/product9.webp",
+  },
+  {
+    _id: 10,
+    name: "Dustpan & Brush",
+    price: 350,
+    thumbnail: "/products/product10.webp",
+  },
+  {
+    _id: 11,
+    name: "Chemex Coffeemaker 3 Cup",
+    price: 450,
+    thumbnail: "/products/product11.webp",
+  },
+  {
+    _id: 12,
+    name: "Chemex Coffeemaker 5 Cup",
+    price: 400,
+    thumbnail: "/products/product12.webp",
+  },
+  {
+    _id: 13,
+    name: "Chemex Coffeemaker 7 Cup",
+    price: 550,
+    thumbnail: "/products/product13.webp",
+  },
+  {
+    _id: 14,
+    name: "Chemex Coffeemaker 9 Cup",
+    price: 500,
+    thumbnail: "/products/product14.webp",
+  },
+];
+
+export default products;
