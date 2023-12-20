8574360696de7505a7d4980c7fd367fba7192ee3
devhasibulislam <devhasibulislam@gmail.com>
2023-12-20 09:26:51 +0600

    footer init

---
+++ client/layouts/main/Footer.jsx
@@ -0,0 +1,26 @@
+/**
+ * Title: Write a program using JavaScript on Footer
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 19, December 2023
+ */
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="max-w-7xl mx-auto px-4 border">
+      This is footer section.
+    </footer>
+  );
+};
+
+export default Footer;
