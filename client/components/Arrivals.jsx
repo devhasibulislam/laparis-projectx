46a39b39b978c4c197d2669c1d3c62bdbb40ead6
devhasibulislam <devhasibulislam@gmail.com>
2023-12-20 18:16:36 +0600

    integrate grub as card

--- client/components/Arrivals.jsx
+++ client/components/Arrivals.jsx
@@ -18,18 +18,12 @@
 import products from "@/data/products";
 import {
   Button,
-  Card,
-  CardBody,
-  CardHeader,
-  Image,
   Link,
   Tab,
   Tabs,
-  Tooltip,
 } from "@nextui-org/react";
 import React from "react";
-import { MdFavoriteBorder } from "react-icons/md";
-import { MdAddShoppingCart } from "react-icons/md";
+import Grub from "./Grub";
 
 const Arrivals = () => {
   function shuffleArray(array) {
@@ -81,39 +75,7 @@ const Arrivals = () => {
             <Tab key={item._id} title={item.label}>
               <div className="grid grid-cols-4 gap-4">
                 {item?.products?.slice(0, 8)?.map((product) => (
-                  <Card key={product?._id} className="">
-                    <CardHeader className="relative group">
-                      <Image
-                        isZoomed
-                        isBlurred
-                        alt="Card background"
-                        className="object-cover rounded-xl"
-                        src={product?.thumbnail}
-                        width={270}
-                      />
-
-                      <div className="absolute top-6 left-6 w-full h-fit z-50 flex flex-row gap-x-2 opacity-0 group-hover:opacity-100 transition-opacity">
-                        <Tooltip showArrow={true} content="Add to Favorite">
-                          <button className="p-1 bg-white shadow border border-black rounded-full">
-                            <MdFavoriteBorder className="h-4 w-4" />
-                          </button>
-                        </Tooltip>
-                        <Tooltip showArrow={true} content="Add to Cart">
-                          <button className="p-1 bg-white shadow border border-black rounded-full">
-                            <MdAddShoppingCart className="h-4 w-4" />
-                          </button>
-                        </Tooltip>
-                      </div>
-                    </CardHeader>
-                    <CardBody>
-                      <p className="text-tiny uppercase font-bold line-clamp-1">
-                        {product?.name}
-                      </p>
-                      <small className="text-default-500">
-                        $<b>{product?.price}</b>
-                      </small>
-                    </CardBody>
-                  </Card>
+                  <Grub key={product?._id} product={product} />
                 ))}
               </div>
             </Tab>
