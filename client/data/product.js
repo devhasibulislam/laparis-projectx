1cff1b6377cb1cc0e60ff87327551ed8e979c219
devhasibulislam <devhasibulislam@gmail.com>
2023-12-21 21:13:02 +0600

    single product mock data init

---
+++ client/data/product.js
@@ -0,0 +1,18 @@
+const product = {
+  _id: 0,
+  name: "Able Brewing System",
+  price: 200,
+  thumbnail: "/products/product1.webp",
+  gallery: [
+    "/products/product2.webp",
+    "/products/product3.webp",
+    "/products/product4.webp",
+    "/products/product5.webp",
+  ],
+  description:
+    "Pellentesque dolor augue, euismod vel orci in, congue sodales nunc. Duis sed auctor dolor, ac facilisis mi. Duis malesuada, arcu ut ultricies pretium, nulla tortor egestas nisl, feugiat dictum mauris elit eu nisi. Donec hendrerit magna a consectetur ullamcorper. Donec est orci, condimentum et eleifend scelerisque, ultrices eget nunc.",
+  category: "printed-t-shirt",
+  size: ["s", "m", "l", "xl", "xxl"],
+};
+
+export default product;
