362e93858beca8b994d3e5918372b45892970618
devhasibulislam <devhasibulislam@gmail.com>
2023-12-22 16:46:07 +0600

    shift from external group to internal nested group

---
+++ client/app/dashboard/(category)/list-categories/page.js
@@ -0,0 +1,23 @@
+/**
+ * Title: Write a program using JavaScript on Page
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 22, December 2023
+ */
+
+import Dashboard from "@/layouts/dashboard/Dashboard";
+import React from "react";
+
+const Page = () => {
+  return <Dashboard>This is list categories page!</Dashboard>;
+};
+
+export default Page;
