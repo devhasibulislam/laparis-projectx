61403c6acf56d47f090f5a2b530dfffce7beca49
devhasibulislam <devhasibulislam@gmail.com>
2023-12-22 10:35:29 +0600

    integrate quantity and sticker item

--- client/app/(collections)/[id]/page.js
+++ client/app/(collections)/[id]/page.js
@@ -81,45 +81,44 @@ const Page = () => {
                 </span>{" "}
                 <Divider orientation="vertical" /> {product?.category}
               </div>
-              <RadioGroup label="Select a Size" orientation="horizontal">
-                {product?.size?.map((item) => (
-                  <Radio key={item} value={item} className="uppercase">
-                    {item}
-                  </Radio>
-                ))}
+              <RadioGroup
+                label="Select a Size"
+                orientation="horizontal"
+                defaultValue={"s"}
+              >
+                {product?.size?.includes("s") && <Radio value="s">S</Radio>}
+                {product?.size?.includes("m") && <Radio value="m">M</Radio>}
+                {product?.size?.includes("l") && <Radio value="l">L</Radio>}
+                {product?.size?.includes("xl") && <Radio value="xl">XL</Radio>}
+                {product?.size?.includes("xxl") && (
+                  <Radio value="xxl">XXL</Radio>
+                )}
               </RadioGroup>
             </article>
 
             <div className="flex flex-col gap-y-4 mt-8">
-              <div class="flex items-center justify-center w-full">
+              <div className="flex flex-row gap-x-2 items-center">
+                <input
+                  type="number"
+                  name="quantity"
+                  id="quantity"
+                  min="1"
+                  placeholder="Enter Quantity"
+                  className="w-full"
+                />
                 <label
-                  for="dropzone-file"
-                  class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                  htmlFor="sticker"
+                  className="w-full border border-black h-full relative flex items-center p-2"
                 >
-                  <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg
-                      class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 20 16"
-                    >
-                      <path
-                        stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-                      />
-                    </svg>
-                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
-                      Click to upload
-                    </p>
-                    <p class="text-xs text-gray-500 dark:text-gray-400">
-                      Select <b>Front & Back Part</b> of your sticker
-                    </p>
-                  </div>
-                  <input id="dropzone-file" type="file" class="hidden" />
+                  <span className="h-full w-full text-sm">
+                    Choose Custom Sticker
+                  </span>
+                  <input
+                    type="file"
+                    name="sticker"
+                    id="sticker"
+                    className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                  />
                 </label>
               </div>
 
