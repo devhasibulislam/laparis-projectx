bd30abc965d64992947dad98dd3bb95be8211a52
devhasibulislam <devhasibulislam@gmail.com>
2023-12-22 12:44:51 +0600

    integrate dashboard sidebar

---
+++ client/layouts/dashboard/Sidebar.jsx
@@ -0,0 +1,22 @@
+/**
+ * Title: Write a program using JavaScript on Sidebar
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 22, December 2023
+ */
+
+import React from "react";
+
+const Sidebar = () => {
+  return <aside className="w-full h-full border col-span-3 p-2 overflow-y-auto">This is sidebar!</aside>;
+};
+
+export default Sidebar;
