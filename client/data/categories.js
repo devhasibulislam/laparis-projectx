3729329af51c38ac96a56e9cbb6c753fa93dfe64
devhasibulislam <devhasibulislam@gmail.com>
2023-12-22 22:34:35 +0600

    categories dummy data init

---
+++ client/data/categories.js
@@ -0,0 +1,119 @@
+const categories = [
+  {
+    _id: 1,
+    name: "Waldo Rosetti",
+    description:
+      "Supplement Pharynx with Autologous Tissue Substitute, Via Natural or Artificial Opening Endoscopic",
+  },
+  {
+    _id: 2,
+    name: "Pietrek Dowtry",
+    description:
+      "Supplement Nasal Turbinate with Synthetic Substitute, Via Natural or Artificial Opening Endoscopic",
+  },
+  {
+    _id: 3,
+    name: "Lissa Fayerman",
+    description: "Removal of Other Device on Right Upper Arm",
+  },
+  {
+    _id: 4,
+    name: "Louise Ferrarini",
+    description: "Dilation of Left Axillary Artery, Percutaneous Approach",
+  },
+  {
+    _id: 5,
+    name: "Shaina Constantine",
+    description:
+      "Drainage of Right Toe Phalangeal Joint with Drainage Device, Percutaneous Approach",
+  },
+  {
+    _id: 6,
+    name: "Hildegarde Dressel",
+    description:
+      "High Dose Rate (HDR) Brachytherapy of Bronchus using Other Isotope",
+  },
+  {
+    _id: 7,
+    name: "Violette Dumberrill",
+    description:
+      "Bypass Thoracic Aorta, Descending to Left Pulmonary Artery with Synthetic Substitute, Open Approach",
+  },
+  {
+    _id: 8,
+    name: "Ludovika Buckbee",
+    description:
+      "Restriction of Left Ureter with Intraluminal Device, Via Natural or Artificial Opening Endoscopic",
+  },
+  {
+    _id: 9,
+    name: "Hagen Linbohm",
+    description:
+      "Revision of Stimulator Lead in Upper Artery, Percutaneous Approach",
+  },
+  {
+    _id: 10,
+    name: "Flynn Dries",
+    description: "Resection of Atrial Septum, Percutaneous Endoscopic Approach",
+  },
+  {
+    _id: 11,
+    name: "Ethelda Leafe",
+    description:
+      "Computerized Tomography (CT Scan) of Bilateral Kidneys using Other Contrast",
+  },
+  {
+    _id: 12,
+    name: "Coraline Saur",
+    description:
+      "Bypass Coronary Artery, Three Arteries from Left Internal Mammary with Synthetic Substitute, Percutaneous Endoscopic Approach",
+  },
+  {
+    _id: 13,
+    name: "Ruddy Hellens",
+    description:
+      "Planar Nuclear Medicine Imaging of Veins using Other Radionuclide",
+  },
+  {
+    _id: 14,
+    name: "Rudd Grimsdith",
+    description:
+      "Replacement of Right Zygomatic Bone with Autologous Tissue Substitute, Percutaneous Endoscopic Approach",
+  },
+  {
+    _id: 15,
+    name: "Omero Mussard",
+    description:
+      "Revision of Autologous Tissue Substitute in Left Glenoid Cavity, Open Approach",
+  },
+  {
+    _id: 16,
+    name: "Denice MacDavitt",
+    description:
+      "Transfer Left Foot Subcutaneous Tissue and Fascia with Skin and Subcutaneous Tissue, Percutaneous Approach",
+  },
+  {
+    _id: 17,
+    name: "Lazare Liddyard",
+    description:
+      "Insertion of External Fixation Device into Right Finger Phalanx, Percutaneous Approach",
+  },
+  {
+    _id: 18,
+    name: "Culley Ciubutaro",
+    description:
+      "Bypass Left Common Iliac Artery to Bilateral Femoral Arteries with Nonautologous Tissue Substitute, Open Approach",
+  },
+  {
+    _id: 19,
+    name: "Pierette Waddams",
+    description: "Release Left Carpal, Percutaneous Endoscopic Approach",
+  },
+  {
+    _id: 20,
+    name: "Saunderson Broseman",
+    description: "Repair Median Nerve, Open Approach",
+  },
+];
+
+export default categories;
