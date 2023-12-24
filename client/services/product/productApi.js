94788ab5fbfdd3aaa1bfb009477b117e6fd45006
devhasibulislam <devhasibulislam@gmail.com>
2023-12-25 00:27:44 +0600

    integrate add product backend

---
+++ client/services/product/productApi.js
@@ -0,0 +1,36 @@
+/**
+ * Title: Write a program using JavaScript on ProductApi
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 24, December 2023
+ */
+
+const { laparisApi } = require("../laparis");
+
+export const productApi = laparisApi.injectEndpoints({
+  endpoints: (build) => ({
+    // add product
+    addProduct: build.mutation({
+      query: (body) => ({
+        url: "/product/create",
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        },
+        body,
+      }),
+
+      invalidatesTags: ["Product"],
+    }),
+  }),
+});
+
+export const { useAddProductMutation } = productApi;
