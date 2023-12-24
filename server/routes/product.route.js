692e3353531713d053685c4686ec7e7cc76ae99e
devhasibulislam <devhasibulislam@gmail.com>
2023-12-25 00:28:05 +0600

    integrate add product backend

---
+++ server/routes/product.route.js
@@ -0,0 +1,43 @@
+/**
+ * Title: Write a program using JavaScript on Product Route
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 24, December 2023
+ */
+
+/* external import */
+const express = require("express");
+
+/* internal import */
+const productController = require("../controllers/product.controller");
+const verify = require("../middleware/verify.middleware");
+const authorize = require("../middleware/authorize.middleware");
+const upload = require("../middleware/upload.middleware");
+
+/* router level connection */
+const router = express.Router();
+
+/* router methods integration */
+
+// add new product
+router.post(
+  "/create",
+  verify,
+  authorize("admin"),
+  upload.fields([
+    { name: "thumbnail", maxCount: 1 },
+    { name: "gallery", maxCount: 5 },
+  ]),
+  productController.addProduct
+);
+
+/* export user router */
+module.exports = router;
---
+++ server/services/product.service.js
@@ -0,0 +1,58 @@
+/**
+ * Title: Write a program using JavaScript on Product Service
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 24, December 2023
+ */
+
+/* internal imports */
+const Product = require("../models/product.model");
+
+/* add new product */
+exports.addProduct = async (req, res) => {
+  const oldProduct = await Product.findOne({ name: req.body.name });
+
+  if (oldProduct) {
+    res.status(400).json({
+      acknowledgement: false,
+      message: "Bad Request",
+      description: "Product already exists",
+    });
+  } else {
+    const product = req.body;
+
+    product.thumbnail = {
+      url: req.files.thumbnail[0].path,
+      public_id: req.files.thumbnail[0].filename,
+    };
+
+    product.gallery = req.files.gallery.map((file) => ({
+      url: file.path,
+      public_id: file.filename,
+    }));
+
+    const newProduct = await Product.create(product);
+
+    if (!newProduct) {
+      res.status(500).json({
+        acknowledgement: false,
+        message: "Internal Server Error",
+        description: "Something went wrong",
+      });
+    } else {
+      res.status(201).json({
+        acknowledgement: true,
+        message: "Created",
+        description: "New product added successfully",
+      });
+    }
+  }
+};
