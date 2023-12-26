f4da9c50d4dac7f727a0e9e7a00f2bf32aa91f22
devhasibulislam <devhasibulislam@gmail.com>
2023-12-26 10:54:12 +0600

    cart functionality integrated

--- client/components/Cart.jsx
+++ client/components/Cart.jsx
@@ -16,14 +16,85 @@
 import React from "react";
 import Grub from "./Grub";
 import products from "@/data/products";
+import { useSelector } from "react-redux";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { Avatar } from "@nextui-org/react";
 
 const Cart = () => {
-  return (
-    <section className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-      {products?.map((product) => (
-        <Grub key={product?._id} product={product} />
-      ))}
-    </section>
+  const user = useSelector((state) => state.user.auth);
+
+  return user?.cart?.length === 0 ? (
+    <>
+      <p className="text-center">No Products Added!</p>
+    </>
+  ) : (
+    <>
+      <div className="relative overflow-x-auto sm:rounded-lg">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Thumbnail
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Name
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Sticker
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Sizes
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Quantity
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {user?.cart?.map(({ product, sticker, quantity, size, price }) => (
+              <tr
+                key={product?._id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              >
+                <td className="px-6 py-4">
+                  <Avatar
+                    src={product?.thumbnail?.url}
+                    alt={product?.thumbnail?.public_id}
+                    size="sm"
+                  />
+                </td>
+                <td className="px-6 py-4">
+                  <span className="whitespace-nowrap w-60 overflow-x-auto block">
+                    {product?.name}
+                  </span>
+                </td>
+                <td className="px-6 py-4">
+                  <Avatar
+                    src={sticker?.url}
+                    alt={sticker?.public_id}
+                    size="sm"
+                  />
+                </td>
+                <td className="px-6 py-4">{size}</td>
+                <td className="px-6 py-4">{quantity}</td>
+                <td className="px-6 py-4">{price}</td>
+                <td className="px-6 py-4 whitespace-nowrap">
+                  <button className="p-1.5 border rounded-full bg-red-500 text-white">
+                    <FaRegTrashAlt className="h-4 w-4" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 };
 
