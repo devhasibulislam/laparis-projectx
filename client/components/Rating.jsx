4b0555eedd7f92811fd19cad6edec50ab295752d
devhasibulislam <devhasibulislam@gmail.com>
2023-12-31 16:27:38 +0600

    star rating modify

--- client/components/Rating.jsx
+++ client/components/Rating.jsx
@@ -22,9 +22,12 @@ import {
 } from "@/services/product/productApi";
 import { toast } from "react-hot-toast";
 import { useForm } from "react-hook-form";
+import { IoIosStarOutline } from "react-icons/io";
+import { IoIosStar } from "react-icons/io";
 
 const Rating = ({ id }) => {
   const [showRating, setShowRating] = useState(false);
+  const [selectedRating, setSelectedRating] = useState(0);
   const { register, handleSubmit, reset } = useForm();
   const [
     addReview,
@@ -77,7 +80,11 @@ const Rating = ({ id }) => {
   ]);
 
   const handleReview = (data) => {
-    addReview({ id, body: data });
+    addReview({ id, body: { feedback: data.feedback, star: selectedRating } });
+  };
+
+  const handleStarClick = (rating) => {
+    setSelectedRating(rating);
   };
 
   return (
@@ -109,7 +116,7 @@ const Rating = ({ id }) => {
                 placeholder="Add your thoughts"
                 className="w-full"
               />
-              <input
+              {/* <input
                 type="number"
                 name="star"
                 id="star"
@@ -118,7 +125,27 @@ const Rating = ({ id }) => {
                 {...register("star", { required: true })}
                 placeholder="Enter Rating (1 to 5)"
                 className="w-full"
-              />
+              /> */}
+
+              <div className="flex flex-row gap-x-2">
+                {[1, 2, 3, 4, 5].map((rating) => (
+                  <button
+                    key={rating}
+                    type="button"
+                    className={`text-yellow-500 ${
+                      rating <= selectedRating ? "text-yellow-500" : ""
+                    }`}
+                    onClick={() => handleStarClick(rating)}
+                  >
+                    {rating <= selectedRating ? (
+                      <IoIosStar className="h-5 w-5" />
+                    ) : (
+                      <IoIosStarOutline className="h-5 w-5" />
+                    )}
+                  </button>
+                ))}
+              </div>
+
               <button
                 type="submit"
                 className="p-2 h-full bg-primary text-white text-sm"
